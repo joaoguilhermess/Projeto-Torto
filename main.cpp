bfb67f1295d0686d1b916c352cc280e9b410c2b1
Jotagê <71993919+joaoguilhermess@users.noreply.github.com>
2025-06-30 15:53:33 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,6 +9,14 @@ int inteiroAleatorio(int min, int max) {
 	return (max - min) * generate(seed) + min;
 }
 
+void embaralharPalavra(TInfo& item) {
+	for (int i = 0; i < item.palavra.length(); i++) {
+		int index = inteiroAleatorio(i, item.palavra.length());
+
+		item.palavra = item.palavra.substr(index, 1) + item.palavra.substr(0, index) + item.palavra.substr(index + 1);
+	}
+}
+
 int main() {
 	TPilha palavras = lerPalavras("./dicionarios/portugues.txt");
 
@@ -16,6 +24,8 @@ int main() {
 
 	for (int i = 0; i < 25; i++) {
 		TInfo item = pegarItem(palavras, inteiroAleatorio(0, palavras.quantidade));
+
+		embaralharPalavra(item);
 
 		std::cout << "Palavra: " << item.palavra;
 		std::cout << "\n";
