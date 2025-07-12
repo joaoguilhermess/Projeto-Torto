4076bb36664092ef1594d1e825f476a5b8b3b17b
Jotagê <71993919+joaoguilhermess@users.noreply.github.com>
2025-07-12 12:34:13 -0300

    Add files via upload

--- v2/main.cpp
+++ v2/main.cpp
@@ -3,6 +3,7 @@
 
 #define COR_BRANCO 255, 255, 255, 255
 #define COR_PRETO 0, 0, 0, 255
+#define COR_VERMELHO 255, 0, 0, 255
 
 #define ESPACAMENTO 50
 #define LARGURA_BORDA 2
@@ -28,6 +29,10 @@ int main() {
 	SDL_Event evento;
 
 	TTF_Text* titulo = TTF_CreateText(texto, fonteTitulo, "JOGO TORTO", 0);
+	int larguraTitulo;
+	int alturaTitulo;
+
+	TTF_GetTextSize(titulo, &larguraTitulo, &alturaTitulo);
 
 	int larguraTela = 0;
 	int alturaTela = 0;
@@ -55,14 +60,14 @@ int main() {
 		SDL_RenderFillRect(tela, &retangulo);
 
 		TTF_SetTextColor(titulo, COR_PRETO);
-		TTF_DrawRendererText(titulo, ESPACAMENTO, ESPACAMENTO);
+		TTF_DrawRendererText(titulo, ESPACAMENTO, 0);
 
-		retangulo.x = ESPACAMENTO;
-		retangulo.y = ESPACAMENTO + LARGURA_BORDA + (50 * 1.3);
+		retangulo.x = 0;
+		retangulo.y = alturaTitulo;
 		retangulo.w = 500;
 		retangulo.h = LARGURA_BORDA;
 
-		SDL_SetRenderDrawColor(tela, COR_PRETO);
+		SDL_SetRenderDrawColor(tela, COR_VERMELHO);
 		SDL_RenderFillRect(tela, &retangulo);
 
 		SDL_RenderPresent(tela);
